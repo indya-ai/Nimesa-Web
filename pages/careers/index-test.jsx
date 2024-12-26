e5bdd0085ec325cc7c73feb9a49d327ab919e16b
Varun Mahajan <vm@indya.ai>
2024-12-26 17:16:16 +0530

    Ensure new files are in root for Vercel

---
+++ pages/careers/index-test.jsx
@@ -0,0 +1,4 @@
+// pages/careers/index.js
+export default function Careers() {
+  return <h1>Careers Page</h1>;
+}
---
+++ pages/index-test.js
@@ -0,0 +1,4 @@
+// pages/index-test.js
+export default function Home() {
+  return <h1>Home Page</h1>;
+}
